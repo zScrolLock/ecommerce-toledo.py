67c9223873c833e15f769af45a57dec73a48c842
zScrolLock <luis.augustocm@hotmail.com>
2022-08-14 20:53:29 -0300

    feat: CRUD User functions

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-## E-Commerce Faculdade Toledo
+## **E-Commerce Faculdade Toledo**
+
+
+ - Luís Augusto C. Mota (2022) - ADS
+### **About the Project**
+Projeto desenvolvido para a matéria Frameworks para Desenvolvimento de Software para o Curso de ADS da Faculdade Toledo de Presidente Prudente
+
+<p align="center">
+    <img src="app/static/img/MER.png" width="600" alt="MER">
+</p>
+
+<hr/>
+
+### **First of All**:
+
+- **1º Clone the Github Repository**
+``` bash
+    test@test~: git clone git@github.com:zScrolLock/ecommerce-toledo.py.git
+    test@test~: cd ecommerce-toledo.py/
+```
+
+- **2º Copy .env.example to .env**
+``` bash
+    test@test~: cp .env.example .env
+```
+<hr/>
+
+### **How to Run the Project**
 
 > To Easily run it: 
 
@@ -7,9 +34,16 @@
     test@test~: echo "alias ecommerce-toledo='source ../myenv/bin/activate && export FLASK_APP=main.py && flask run --reload --debugger'" >> $HOME/.bash_aliases
     test@test~: ecommerce-toledo
 ```
+> zsh Case to Easily run it:
+
+```bash
+    test@test~: cd app/
+    test@test~: source ~/.zshrc
+    test@test~: echo "alias ecommerce-toledo='source ../myenv/bin/activate && export FLASK_APP=main.py && flask run --reload --debugger'" >> ~/.zshrc
+    test@test~: ecommerce-toledo
+```
 
 > Common Method to run it:
-
 
 ```bash
     test@test~: cd app/
@@ -18,4 +52,13 @@
     (myenv) test@test~: flask run --reload
 ```
 
- - Luís Augusto C. Mota (2022) - ADS
+<hr/>
+
+### **Error Codes Explain**:
+
+| Code | Explain  |
+|---|---|
+|  404 | Not found  |
+|  401 | Unauthorized  |
+
+<hr />
--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
-from flask import Flask, make_response
-from markupsafe import escape
-from flask import render_template
-from flask import request
+from flask import Flask, make_response, abort, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask import redirect
-from flask import url_for
-from flask import jsonify
+from dotenv import load_dotenv
+from pathlib import Path
+import jwt
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Luis_147147@localhost:3306/toledo_ecommerce'
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+app.debug = True
+app.config['SECRET_JWT_KEY'] = os.getenv("SECRET_JWT_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASS")}@localhost:3306/{os.getenv("DATABASE_DB")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -23,21 +25,43 @@ class Users(db.Model):
     role = db.Column('role', db.String(256), default = "User")
     shops_id = db.Column('shops_id', db.Integer)
 
-    def __init__(this, name, email, username, password, salt, role, shops_id):
+    # Constructor
+    def __init__(this, name, email, username, password, salt, shops_id):
         this.name = name
         this.email = email
         this.username = username
         this.password = password
         this.salt = salt
-        this.role = role
         this.shops_id = shops_id
 
+    # ToString Method
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+# Validate user With JWT Token
+def checkUser(token):
+    if not token:
+        return False
+
+    credentials = jwt.decode(token, app.config['SECRET_JWT_KEY'])
+    result = Users.query.filter(Users.username == credentials['username'], Users.id == credentials['id']).one()
+
+    if result == None:
+        return False
+    else:
+        return True
+
+@app.errorhandler(404)
+def notFoundPage(err):
+    return render_template('notFound.html', error = err)
+
+@app.errorhandler(401)
+def notFoundPage(err):
+    return render_template('unauthorized.html', error = err)
+
 @app.route("/")
 def index():
-    return render_template('home.html', username=request.cookies.get('username'), users = Users.query.all())
+    return render_template('home.html', username=request.cookies.get('username'), users = Users.query.all(), token = request.cookies.get('token'))
 
 @app.route("/login")
 def loginPage():
@@ -45,7 +69,7 @@ def loginPage():
 
 @app.route("/user-login", methods=['POST'])
 def login():
-    result = Users.query.filter(Users.username == request.form.get('username'), Users.password == request.form.get('password')).first()
+    result = Users.query.filter(Users.username == request.form.get('username'), Users.password == request.form.get('password')).one()
 
     if(result == None):
         return jsonify(
@@ -53,8 +77,13 @@ def login():
             code = 404,
             message = "User not Found"
         )
-
-    return result.as_dict()
+    token = jwt.encode({'username': result.username, 'id': result.id, 'role': result.role}, app.config['SECRET_JWT_KEY'])
+    
+    cookie = make_response(render_template('profile.html', user = result))
+    cookie.set_cookie('username', result.name)
+    cookie.set_cookie('token', token)
+    
+    return cookie
 
 @app.route("/register")
 def registerPage(): 
@@ -62,7 +91,7 @@ def registerPage():
 
 @app.route("/register-user", methods=['POST'])
 def registerUser():
-    newUser = Users(request.form.get('name'), request.form.get('email'), request.form.get('username'), request.form.get('password'), '', '', None)
+    newUser = Users(request.form.get('name'), request.form.get('email'), request.form.get('username'), request.form.get('password'), '', None)
     db.session.add(newUser)
     db.session.commit()
 
@@ -71,27 +100,76 @@ def registerUser():
 @app.route("/profile/details/")
 @app.route("/profile/details")
 def userProfilePage(): 
-    return render_template('profile.html', username=request.cookies.get('username'))
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
 
+    user = Users.query.get(jwt.decode(request.cookies.get('token'), app.config['SECRET_JWT_KEY'])['id'])
+
+    if not user:
+        return jsonify({'message': 'user not found'})
+
+    return render_template('profile.html', user = user)
+
+
+@app.route("/profile/details/update", methods=['POST'])
+def updateUser():
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
+
+    decodedInfos = jwt.decode(request.cookies.get('token'), app.config['SECRET_JWT_KEY'])
+
+    user = Users.query.get(decodedInfos['id'])
+    user.name = request.form.get('name') or user.name
+    
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('userProfilePage'))
+
+@app.route("/profile/details/delete", methods=['POST'])
+def deleteUser():
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
+
+@app.route("/logout-user", methods=['POST'])
+def logoutUser(): 
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
+
+    cookie = make_response(redirect("/"))
+    cookie.delete_cookie('token')
+    cookie.delete_cookie('username')
+
+    return cookie
+
+@app.route("/shop/")
 @app.route("/shop")
 def shopPage():
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
+
     return render_template('shops.html')
 
 @app.route("/shop/register")
 def registerShopPage(): 
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
+
     return render_template('registerShop.html')
 
 @app.route("/shop/edit")
 def updateShopPage():
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
+
     return render_template('editShop.html')
 
 @app.route("/shop/report")
 def reportsSalesShopPage():
-    return render_template('reportSales.html')
+    if not checkUser(request.cookies.get('token')):
+        return abort(401)
 
-@app.route("/product/<product_id>")
-def productDetailPage():
-    return render_template('product.html')
+    return render_template('reportSales.html')
 
 if __name__ == 'main':
     db.create_all()
