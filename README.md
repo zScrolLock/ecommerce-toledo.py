bb473264832e4b3fc1309936680212484b69049d
zScrolLock <luis.augustocm@hotmail.com>
2022-08-14 17:00:18 -0300

    feat: Update to ORM Method

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 ## E-Commerce Faculdade Toledo
 
+> To Easily run it: 
+
+```bash
+    test@test~: cd app/
+    test@test~: echo "alias ecommerce-toledo='source ../myenv/bin/activate && export FLASK_APP=main.py && flask run --reload --debugger'" >> $HOME/.bash_aliases
+    test@test~: ecommerce-toledo
+```
+
+> Common Method to run it:
+
+
 ```bash
     test@test~: cd app/
     test@test~: export FLASK_APP=main.py
--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from flask import redirect
 from flask import url_for
+from flask import jsonify
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Luis_147147@localhost:3306/toledo_ecommerce'
@@ -16,71 +17,80 @@ class Users(db.Model):
     id = db.Column('id', db.Integer, primary_key = True)
     name = db.Column('name', db.String(256))
     email = db.Column('email', db.String(256))
+    username = db.Column('username', db.String(256))
     password = db.Column('password', db.String(256))
     salt = db.Column('salt', db.String(256))
-    role = db.Column('role', db.String(256))
+    role = db.Column('role', db.String(256), default = "User")
     shops_id = db.Column('shops_id', db.Integer)
 
-    def __init__(this, name, email, password, salt, role, shops_id):
+    def __init__(this, name, email, username, password, salt, role, shops_id):
         this.name = name
         this.email = email
+        this.username = username
         this.password = password
         this.salt = salt
         this.role = role
         this.shops_id = shops_id
 
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 @app.route("/")
 def index():
     return render_template('home.html', username=request.cookies.get('username'), users = Users.query.all())
 
+@app.route("/login")
+def loginPage():
+    return render_template('login.html')
+
+@app.route("/user-login", methods=['POST'])
+def login():
+    result = Users.query.filter(Users.username == request.form.get('username'), Users.password == request.form.get('password')).first()
+
+    if(result == None):
+        return jsonify(
+            ok = False,
+            code = 404,
+            message = "User not Found"
+        )
+
+    return result.as_dict()
+
 @app.route("/register")
-def register(): 
+def registerPage(): 
     return render_template('register.html')
 
 @app.route("/register-user", methods=['POST'])
 def registerUser():
-    newUser = Users(request.form.get('name'), request.form.get('email'), request.form.get('password'), '', '', None)
+    newUser = Users(request.form.get('name'), request.form.get('email'), request.form.get('username'), request.form.get('password'), '', '', None)
     db.session.add(newUser)
     db.session.commit()
 
     return redirect(url_for('index'))
 
-@app.route("/profile/<username>")
-def username(username):
-    cookie = make_response(f"""
-        <div>
-            <span>
-                <b>Hello {username} - <a href='/'>Back to Home</a></b>
-            </span>
-        </div>
-    """)
-
-    cookie.set_cookie('username', username)
-    return cookie
-
 @app.route("/profile/details/")
 @app.route("/profile/details")
-def userProfile(): 
+def userProfilePage(): 
     return render_template('profile.html', username=request.cookies.get('username'))
 
 @app.route("/shop")
-def shop():
+def shopPage():
     return render_template('shops.html')
 
 @app.route("/shop/register")
-def registerShop(): 
+def registerShopPage(): 
     return render_template('registerShop.html')
 
 @app.route("/shop/edit")
-def updateShop():
+def updateShopPage():
     return render_template('editShop.html')
 
 @app.route("/shop/report")
-def reportsSalesShop():
+def reportsSalesShopPage():
     return render_template('reportSales.html')
 
 @app.route("/product/<product_id>")
-def productDetail():
+def productDetailPage():
     return render_template('product.html')
 
 if __name__ == 'main':
