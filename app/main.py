d89486a8e1f4805ab7dcc5a6a46016aecdc588a8
zScrolLock <luis.augustocm@hotmail.com>
2022-08-14 12:05:39 -0300

    feat: ORM & Styles

--- app/main.py
+++ app/main.py
@@ -2,12 +2,36 @@ from flask import Flask, make_response
 from markupsafe import escape
 from flask import render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Luis_147147@localhost:3306/toledo_ecommerce'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Users(db.Model): 
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column('name', db.String(256))
+    email = db.Column('email', db.String(256))
+    password = db.Column('password', db.String(256))
+    salt = db.Column('salt', db.String(256))
+    role = db.Column('role', db.String(256))
+    shops_id = db.Column('shops_id', db.Integer)
+
+    def __init__(this, name, email, password, salt, role, shops_id):
+        this.name = name
+        this.email = email
+        this.password = password
+        this.salt = salt
+        this.role = role
+        this.shops_id = shops_id
 
 @app.route("/")
 def index():
-    return render_template('home.html',  username=request.cookies.get('username'))
+    return render_template('home.html', username=request.cookies.get('username'), users = Users.query.all())
 
 @app.route("/register")
 def register(): 
@@ -15,7 +39,11 @@ def register():
 
 @app.route("/register-user", methods=['POST'])
 def registerUser():
-    return request.form
+    newUser = Users(request.form.get('name'), request.form.get('email'), request.form.get('password'), '', '', None)
+    db.session.add(newUser)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 @app.route("/profile/<username>")
 def username(username):
@@ -26,6 +54,7 @@ def username(username):
             </span>
         </div>
     """)
+
     cookie.set_cookie('username', username)
     return cookie
 
@@ -53,3 +82,6 @@ def reportsSalesShop():
 @app.route("/product/<product_id>")
 def productDetail():
     return render_template('product.html')
+
+if __name__ == 'main':
+    db.create_all()
